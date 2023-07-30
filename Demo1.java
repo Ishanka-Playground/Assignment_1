f04ba064b44a2e86da616882e89f0ab2545bfecb
Ishanka Dilshan <ishankadilshan64@gmail.com>
2023-07-30 11:46:02 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        String currency = "$";
+        double balance = 123456.7890;
+
+        System.out.printf("%s%,.2f\n", currency,balance);
+    
+    }
+}
