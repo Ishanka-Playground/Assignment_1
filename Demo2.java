042e6e403e6146d126487826af0b517bc807837c
Ishanka Dilshan <ishankadilshan64@gmail.com>
2023-07-30 13:03:00 +0530

    Complete Task 3

--- Demo2.java
+++ Demo2.java
@@ -2,6 +2,8 @@ public class Demo2 {
     public static void main(String[] args) {
 
         System.out.printf ("%.3f\n", Math.PI);
+
+
     }
     
 }
---
+++ Demo3.java
@@ -0,0 +1,21 @@
+public class Demo3 {
+
+    public static void main(String[] args) {
+        int num1 = 123;
+        int num2 = 456;
+        int num3 = 7890;
+
+        String colour = "\033[32m";
+
+        System.out.printf("(\033[33m%s\033[0m) %s-%s\n", num1,num2,num3); // colour the area code
+
+        System.out.printf("(\033[33;7m%s\033[0m) %s-%s\n", num1,num2,num3); // colour the area code
+
+
+
+
+
+
+    }
+    
+}
